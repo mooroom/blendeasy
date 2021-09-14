b35cf5482b951661d37f10157f9c55e3d4ad8595
mooroom <61869246+mooroom@users.noreply.github.com>
2021-09-15 01:36:52 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+import logo from "./img/logo.svg";
+import logo_coupang from "./img/logo_coupang_w.png";
+import logo_baemin from "./img/logo_baemin_w.png";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
+const Nav = styled.div`
+  position: fixed;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  img {
+    height: 30px;
+  }
+`;
+
+const TextCon = styled.div`
+  position: absolute;
+  top: 100px;
+  left: 20px;
+
+  h1 {
+    font-family: condor, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    opacity: 0;
+  }
+
+  h1.first {
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+    animation-name: ${fadeIn};
+    animation-fill-mode: forwards;
+  }
+
+  h1.second {
+    animation-delay: 1s;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+    animation-name: ${fadeIn};
+    animation-fill-mode: forwards;
+  }
+`;
+
+const BtnCon = styled.div`
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DeliveryBtn = styled.button`
+  outline: none;
+  border-style: none;
+  background: black;
+  color: white;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 20px;
+  }
+
+  /* & + & {
+    margin-top: 10px;
+  } */
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Nav>
+        <img src={logo} alt="logo" />
+      </Nav>
+      <TextCon>
+        <h1 className="first">Take it easy,</h1>
+        <h1 className="second">blendeasy.</h1>
+      </TextCon>
+      <BtnCon>
+        <DeliveryBtn
+          onClick={() => window.open("https://baemin.me/bZG-Fc-sEh")}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img src={logo_baemin} alt="baemin" />
+        </DeliveryBtn>
+        <DeliveryBtn
+          onClick={() =>
+            window.open(
+              "https://web.coupangeats.com/share?storeId=335915&dishId&key=fbe62956-8357-4417-b8c0-723f93d02968"
+            )
+          }
+        >
+          <img src={logo_coupang} alt="coupang" />
+        </DeliveryBtn>
+      </BtnCon>
+    </Container>
   );
 }
 
